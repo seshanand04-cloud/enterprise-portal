b0694442f4f86f23056650e2947010016ca0fcc6
Seshanand <seshanand04@gmail.com>
2026-07-03 13:28:16 +0530

    Add all 8 service projects and solution file

---
+++ shared/Common.Library/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Common.Library;
+
+public class Class1
+{
+
+}
---
+++ src/AdminPortal.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AdminPortal.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
